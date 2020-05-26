54af6227350acb9eabb7ff881da891ff00d93ceb
Jay Sullivan <jaytsully@gmail.com>
2020-05-26 08:54:55 -0500

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+// Serve up index.html for all paths not handled
+// Same as historyApiFallback: true in webpack config
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is up!');
+});
